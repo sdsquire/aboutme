65deb2c2d8078a34eed5caaf4c8562c7130754e6
Samuel Squires <sdsquires@pop-os.localdomain>
2023-12-27 15:52:28 -0700

    Small debugging problem

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('header.html')
+    const scriptSrc = document.currentScript.src;
+    const baseUrl = scriptSrc.substring(0, scriptSrc.lastIndexOf('/') + 1);
+    fetch(baseUrl + '../header.html')
         .then(response => response.text())
         .then(data => document.getElementById('header-placeholder')
             .innerHTML = data)
